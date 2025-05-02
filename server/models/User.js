9bda68eb7c6f3448da56ef7b13c14a2a2dff72b9
Sarang Kadukar <116329456+Sarang9975@users.noreply.github.com>
2025-05-02 22:18:22 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -36,4 +36,4 @@ const userSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema); 
+module.exports = mongoose.model('PhoneUser', userSchema, 'phoneUsers'); 
